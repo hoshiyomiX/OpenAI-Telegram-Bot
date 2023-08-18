80fd011c7216f3c65685666028ae27300aed4d7a
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2023-08-18 16:33:11 +0800

    Update openai-telegram.py
    - Fixed spam in group
    - Use /ask to trigger answer

--- openai-telegram.py
+++ openai-telegram.py
@@ -1,22 +1,27 @@
 # Telegram Bot OpenAI ChatGPT version Python by Wannazid
+# Remake by RiProG-id
 import openai
 from aiogram import Bot, Dispatcher, types, executor
 
-bot_tkn = 'ISI BOT TOKEN YG ADA DI BOTFATHER'
-openai.api_key = 'ISI API KEY DARI OPENAI'
+bot_tkn = 'TOKEN_BOT_TELEGRAM_ANDA'
+openai.api_key = 'KUNCI_API_OPENAI_ANDA'
 
 bot = Bot(token=bot_tkn)
 dp = Dispatcher(bot=bot)
 
-@dp.message_handler(commands=['start','help'])
+@dp.message_handler(commands=['start', 'help'])
 async def user_come(pesan: types.Message):
-	await pesan.answer('Selamat Datang, silahkan tanyakan apa saja yang ingin anda ketahui')
-	
-@dp.message_handler(lambda message: message.text)
+    await pesan.answer('Selamat Datang, silahkan tanyakan apa saja yang ingin anda ketahui')
+
+@dp.message_handler(lambda message: message.text.startswith('/ask'))
 async def ai_answer(message: types.Message):
-	respon = openai.Completion.create(model='text-davinci-003', prompt=message.text, temperature=0, max_tokens=1000)
-	parse = respon['choices'][0]['text']
-	await message.reply(parse)
-	
-print('Bot berjalan !')	
+    prompt = message.text[len('/ask'):].strip()
+    if prompt:
+        respon = openai.Completion.create(model='text-davinci-003', prompt=prompt, temperature=0.7, max_tokens=100)
+        parse = respon['choices'][0]['text']
+        await message.reply(parse)
+    else:
+        await message.reply("Mohon masukkan pertanyaan setelah '/ask'")
+
+print('Bot berjalan !')
 executor.start_polling(dp)
