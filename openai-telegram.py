36afd76a9c96fd9f4b5a7e83cd70a1a7e526c940
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2023-10-22 09:33:55 +0800

    Update openai-telegram.py
    Fix bot module error

--- openai-telegram.py
+++ openai-telegram.py
@@ -1,4 +1,4 @@
-# Telegram Bot OpenAI ChatGPT versi Python oleh Wannazid
+~# Telegram Bot OpenAI ChatGPT versi Python oleh Wannazid
 # Dibuat ulang oleh RiProG-id
 
 import openai
@@ -10,9 +10,10 @@ keep_alive()
 bot_tkn = 'TOKEN_BOT_TELEGRAM_ANDA'
 openai.api_key = 'KUNCI_API_OPENAI_ANDA'
 
-bbot = Bot(token=bot_tkn)
+bot = Bot(token=bot_tkn)
 dp = Dispatcher(bot=bot)
 
+# Dictionary to store questions asked by each user
 user_questions = {}
 
 @dp.message_handler(commands=['start', 'help'])
@@ -24,7 +25,7 @@ async def process_ask(message: types.Message):
     user_id = message.from_user.id
     question = message.text[len('/tanya '):].strip()
     user_questions[user_id] = [question]
-    if len(question.split()) > 1:
+    if len(question.split()) > 1:  # Check if there is more than one word in the question
         bot_response = await get_bot_response(question)
         await message.reply(bot_response)
     else:
@@ -36,7 +37,7 @@ async def process_reply(message: types.Message):
     if user_id in user_questions:
         question = message.reply_to_message.text + " " + message.text
         user_questions[user_id].append(question)
-        if len(question.split()) > 1:
+        if len(question.split()) > 1:  # Check if there is more than one word in the question
             bot_response = await get_bot_response(question)
             await message.reply(bot_response)
         else:
@@ -48,3 +49,4 @@ async def get_bot_response(question):
 
 print('Bot is running!')
 executor.start_polling(dp)
+
